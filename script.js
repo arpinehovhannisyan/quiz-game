1ca59deb081b38e6279d4f981c87abe1bddc9166
arpinehovhannisyan <arpinehovh1994@gmail.com>
2022-03-22 01:43:39 +0400

    avelacnel nor harcer

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-let data = {
-    title: '1. Which language runs in a web browser?',
-    options: ['im hancharex parasxan', 'A2', 'A3', 'A4'],
-    answer: 'A2'
-}
+let questions = [
+    {
+        title: `Чему равна длина arr.length массива arr?
+<pre>
+let arr = [];
+arr[1] = 1;
+arr[3] = 33;
+</pre>`,
+        options: ['1', '2', '3', '4'],
+        answer: '4'
+    },
+    {
+        title: `Что выведет этот код?
+<pre>
+function F() { return F; }
+alert( new F() instanceof F );
+alert( new F() instanceof Function );
+</pre>`,
+        options: [
+            'false, false',
+            'false, true',
+            'true, false',
+            'true, true'
+        ],
+        answer: 'false, true'
+    }
+];
+
+let currentIndex = 0;
 
 let containerEl = document.getElementById('container');
 
-function addQuestion(data) {
+function addQuestion(index) {
+    let data = questions[index]
+
     let titleEl = document.createElement('div');
-    titleEl.innerText = data.title;
+    titleEl.innerHTML = `${index + 1}. ${data.title}`;
     titleEl.classList.add('question');
 
     let optionsWrapper = document.createElement('div');
@@ -25,11 +51,16 @@ function addQuestion(data) {
         optionsWrapper.append(wrapperEl);
     })
 
+    containerEl.innerHTML = '';
     containerEl.append(titleEl, optionsWrapper);
 }
 
-addQuestion(data)
+addQuestion(currentIndex)
 
 
+document.addEventListener('click', () => {
+    currentIndex++;
+    addQuestion(currentIndex)
+})
 
 
